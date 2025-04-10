9a10f726710aded393b2c61c1e9c64d28eb0d9d9
Axyss <alexnieto201@gmail.com>
2025-04-10 19:43:57 +0200

    add: b&w game icon effect

--- src/components/ClaimButton.vue
+++ src/components/ClaimButton.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import { claimGenshinReward } from '../main.ts'
 const rewardsClaimed = ref(false)
-
-function claimRewards() {
-  fetch("https://sg-hk4e-api.hoyolab.com/event/sol/sign", {
-    method: "POST",
-    body: JSON.stringify({act_id: "e202102251931481"}),
-  }).then((res) => {
-    if (!res.ok) {
-      // If response status is not ok, throw an error
-      return Promise.reject(`Failed to claim rewards: ${res.statusText}`);
-    }
-    return res.json();  // Parse the response as JSON
-  })
-      .then((data) => {
-        console.log(data);  // Log the parsed result
-        rewardsClaimed.value = true;  // Set rewardsClaimed to true
-        setTimeout(() => {rewardsClaimed.value = false}, 1500)
-      })
-      .catch((error) => {
-        console.error("Error claiming rewards:", error);  // Log errors
-      });
-}
 </script>
 
 <template>
-  <button type="button" class="btn btn-gradient btn-primary w-30" @click="claimRewards">
+  <button type="button" class="btn btn-gradient btn-primary w-30" @click="claimGenshinReward">
     <span v-if="rewardsClaimed" class="loading loading-spinner"></span>
     <span v-else class="icon-[tabler--gift] size-5"></span>
     Claim
--- src/components/GameOption.vue
+++ src/components/GameOption.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-  const props = defineProps({
-    name: String,
-    icon: String,
-  })
+import { ref } from 'vue'
+const props = defineProps({
+  name: String,
+  icon: String,
+})
+const isEnabled = ref(false)
 </script>
 
 <template>
@@ -10,14 +12,15 @@
     <label class="flex items-center justify-between gap-3 p-3">
       <div class="avatar">
         <div class="size-10 rounded-md">
-          <img :src="props.icon" alt="avatar" />
+          <img :src="props.icon" :class="{grayscale: !isEnabled}" alt="avatar" />
         </div>
       </div>
+
       <div>
         <span class="label-text text-base py-0">{{ props.name }}</span>
         <span class="label-text text-success text-xs py-0">Claimed today</span>
       </div>
-      <input type="checkbox" class="switch switch-primary" />
+      <input type="checkbox" v-model="isEnabled" class="switch switch-primary" />
     </label>
   </li>
 </template>
