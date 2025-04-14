7171f63a8612590578382e4bedd172b871f1dc52
Axyss <alexnieto201@gmail.com>
2025-04-14 16:16:50 +0200

    fix: recursion problem with local storage

--- src/persistence.ts
+++ src/persistence.ts
@@ -3,13 +3,15 @@ type Config = Record<string, any>
 export async function getConfig(namespace: string): Promise<Config> {
     return new Promise((resolve) => {
         chrome.storage.local.get(namespace, (result) => {
-            resolve(result[namespace])
+            resolve(result[namespace] || {})
         })
     })
 }
 
 export async function setConfig(namespace: string, config: Config): Promise<void> {
-     new Promise(() => {
-        chrome.storage.local.set({ [namespace]: [config] }, () => {})
+     new Promise<void>((resolve) => {
+        chrome.storage.local.set({ [namespace]: config }, () => {
+          resolve();
+        })
     })
 }
