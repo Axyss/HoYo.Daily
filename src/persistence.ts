5ea3ff293eb152d71cf0e353b429ff0a0ad08784
Axyss <alexnieto201@gmail.com>
2025-04-14 05:28:52 +0200

    refactor: more abstract persistence api

--- src/persistence.ts
+++ src/persistence.ts
@@ -1,33 +1,15 @@
-import { reactive } from 'vue'
-
-export const configTemplate = {
-    genshin: {
-        enabled: true,
-    },
-    hsr: {
-        enabled: true,
-    },
-    zzz: {
-        enabled: true,
-    }
-}
-export const config = reactive(configTemplate)
-
-chrome.storage.local.set(configTemplate)
-
 type Config = Record<string, any>
-let configSingleton: Config | null = null
 
-export async function getConfig(): Promise<Config> {
-    if (configSingleton) return configSingleton
-
-    const data: Config = await new Promise((resolve) => {
-        chrome.storage.local.get(null, (result) => {
-            resolve(result)
+export async function getConfig(namespace: string): Promise<Config> {
+    return new Promise((resolve) => {
+        chrome.storage.local.get(namespace, (result) => {
+            resolve(result[namespace])
         })
     })
-    configSingleton = reactive({...configTemplate, ...data})
-    return configSingleton
 }
 
-
+export async function setConfig(namespace: string, config: Config): Promise<void> {
+     new Promise(() => {
+        chrome.storage.local.set({ [namespace]: [config] }, () => {})
+    })
+}
