4a0cb4c329b2d7254fc9903ecda02b85ec0197bf
Axyss <alexnieto201@gmail.com>
2025-04-15 01:22:16 +0200

    refactor: simpler persistence

--- src/persistence.ts
+++ src/persistence.ts
@@ -1,17 +1,9 @@
 type Config = Record<string, any>
 
-export async function getConfig(namespace: string): Promise<Config> {
-    return new Promise((resolve) => {
-        chrome.storage.local.get(namespace, (result) => {
-            resolve(result[namespace] || {})
-        })
-    })
+export async function getStorage(namespace: string): Promise<Config> {
+     return (await chrome.storage.local.get(namespace))[namespace] || {}
 }
 
-export async function setConfig(namespace: string, config: Config): Promise<void> {
-     new Promise<void>((resolve) => {
-        chrome.storage.local.set({ [namespace]: config }, () => {
-          resolve();
-        })
-    })
+export async function setStorage(namespace: string, config: Config): Promise<void> {
+     await chrome.storage.local.set({ [namespace]: config })
 }
