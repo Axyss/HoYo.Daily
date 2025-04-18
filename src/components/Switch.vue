892e14ab940b3f7fbd068e3ed60fbde5df3d331d
Axyss <alexnieto201@gmail.com>
2025-04-18 19:52:25 +0200

    fix: switch reactivity not being two-way

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
-const props = defineProps(['modelValue'])
-const emit = defineEmits(['update:modelValue'])
-const state = ref(false)
+const model = defineModel<boolean>()
 </script>
 
 <template>
@@ -11,12 +7,14 @@ const state = ref(false)
     <input
       type="checkbox"
       class="sr-only peer"
-      :value="props.modelValue"
-      @click="state = !state"
-      @input="emit('update:modelValue', state)"
+      v-model="model"
     >
-    <div class="w-11 h-6 bg-gray-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"></div>
+    <div
+      class="w-11 h-6 bg-gray-700 peer-focus:outline-none rounded-full
+             peer peer-checked:after:translate-x-full peer-checked:after:border-white
+             after:content-[''] after:absolute after:top-[2px] after:left-[2px]
+             after:bg-white after:border-gray-300 after:border after:rounded-full
+             after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"
+    ></div>
   </label>
 </template>
-
-<style scoped></style>
