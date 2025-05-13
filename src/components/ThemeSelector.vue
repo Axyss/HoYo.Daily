5b5192b37be2a289adee0377088d7659a6961dc2
Axyss <alexnieto201@gmail.com>
2025-05-13 03:47:01 +0200

    add: initial ui theme depends on system settings

--- src/components/ThemeSelector.vue
+++ src/components/ThemeSelector.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
+import { ref, watch, onMounted } from 'vue';
 
+const isDarkMode = ref(false);
+
+onMounted(() => {
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+  isDarkMode.value = prefersDark.matches;
+});
+
+watch(isDarkMode, (newValue) => {
+  const theme = newValue ? 'black' : 'light';
+  document.documentElement.setAttribute('data-theme', theme);
+});
 </script>
 
 <template>
   <label class="swap swap-rotate text-base-content/40 hover:text-primary p-2 ml-auto rounded-full duration-200 cursor-pointer">
-    <input type="checkbox" value="black" class="theme-controller" />
-    <span class="swap-off icon-[lucide--sun] size-5"></span>
-    <span class="swap-on icon-[lucide--moon] size-5"></span>
+    <input type="checkbox" class="theme-controller" :checked="isDarkMode" @change="isDarkMode = !isDarkMode" />
+    <span class="swap-on icon-[lucide--sun] size-5"></span>
+    <span class="swap-off icon-[lucide--moon] size-5"></span>
   </label>
 </template>
 
 <style scoped>
-
 </style>
