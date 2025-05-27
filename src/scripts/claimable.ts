329d3c53f4e17411c6be13f9ff004919c5541583
Axyss <alexnieto201@gmail.com>
2025-05-27 23:57:05 +0200

    refactor: we are taking the easy route

--- src/scripts/claimable.ts
+++ src/scripts/claimable.ts
@@ -6,13 +6,6 @@ export async function claimGenshinRewards(): Promise<Response> {
   });
 }
 
-export async function listGenshinRewards(): Promise<Response> {
-  const response = await fetch(
-    "https://sg-hk4e-api.hoyolab.com/event/sol/home?act_id=e202102251931481&lang=en-us",
-  );
-  return (await response.json()).data.awards;
-}
-
 export async function getGenshinClaimCount(): Promise<number> {
   const response = await fetch(
     "https://sg-hk4e-api.hoyolab.com/event/sol/resign_info?act_id=e202102251931481&lang=en-us",
@@ -29,13 +22,6 @@ export async function claimStarRailRewards(): Promise<Response> {
   });
 }
 
-export async function listStarRailRewards(): Promise<Response> {
-  const response = await fetch(
-    "https://sg-public-api.hoyolab.com/event/luna/hkrpg/os/home?lang=en-us&act_id=e202303301540311",
-  );
-  return (await response.json()).data.awards;
-}
-
 export async function getStarRailClaimCount(): Promise<number> {
   const response = await fetch(
     "https://sg-public-api.hoyolab.com/event/luna/hkrpg/os/info?lang=en-us&act_id=e202303301540311",
@@ -50,13 +36,6 @@ export async function claimZenlessRewards(): Promise<Response> {
     method: "POST",
     body: JSON.stringify({ act_id: "e202406031448091", lang: "en-us" }),
   });
-}
-
-export async function listZenlessRewards(): Promise<Response> {
-  const response = await fetch(
-    "https://sg-public-api.hoyolab.com/event/luna/zzz/os/info?lang=en-us&act_id=e202406031448091",
-  );
-  return (await response.json()).data.awards;
 }
 
 export async function getZenlessClaimCount(): Promise<number> {
