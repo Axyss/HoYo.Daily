7b0f95c3f3f30116249ac11ee61bebdf8e9938f6
Axyss <alexnieto201@gmail.com>
2025-06-20 01:46:28 +0200

    Revert "fix: confetti in firefox now 'kind of' works"
    This reverts commit 05ae7dedecabbdc34c4e19dc1ccd2473002377ab.

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -2,10 +2,6 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 
-export function isFirefox(): boolean {
-  return navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
-}
-
 // Timing functions
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -30,7 +26,6 @@ export function happenedMoreThanADayAgo(epoch: number): boolean {
   return dayjs().unix() - epoch >= 86400;
 }
 
-// Notifications
 export enum NotificationState {
   DISABLED = "disabled",
   MINIMAL = "minimal",
